d345e0c7c909412bebe6563231c20ff69f2ff15d
imbjdd <b0bb1e@protonmail.com>
2024-01-19 23:24:31 +0100

    feat: KISS, remove community parts

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -23,14 +23,17 @@ export default async function Index() {
     .from("list")
     .select()
     .filter('date_end', 'gte', todayDate)
+    .limit(8)
 
   if(!hackathons) hackathons = []
 
+// perhaps in a future version
+/*
   const users = Array(4).fill({
     avatar: 'https://bjdd.me/picture.jpg',
     name: 'Bjdd'
   })
-
+*/
   return (
     <main className="">
       <Search />
@@ -61,7 +64,7 @@ export default async function Index() {
             </div>
           </div>
       </section>
-
+{/*
       <div className="max-w-7xl mx-auto px-4 pb-4 flex items-center">
        <h1 className="text-3xl font-bold ">Top Builders 🏗️</h1>
        <div className="ml-4 h-0.5 grow bg-neutral-100 dark:bg-neutral-800"></div>
@@ -81,27 +84,13 @@ export default async function Index() {
             </div>
           )})}
        </div>
-       <div className="inline-block pt-2 flex flex-nowrap">
-          {users.map((user,i) => { return (
-            <div key={i} className="relative flex py-2 flex-col rounded-xl overflow-hidden slide no-wrap">
-              <Image
-                src="https://bjdd.me/picture.jpg"
-                height={200}
-                width={200}
-                className="w-96 grayscale brightness-50 max-w-fit rounded-xl mr-2 md:mr-6"
-                alt={`Profile picture of ${user.name}`}
-              />
-              <div className="absolute text-white bottom-8 left-4 font-bold">{user.name}</div>
-            </div>
-          )})}
-       </div>
       </div>
 
       <div className="max-w-7xl mx-auto px-4 my-8 flex items-center">
        <h1 className="text-3xl font-bold ">Explore Projects 🦄</h1>
        <div className="ml-4 h-0.5 grow bg-neutral-100 dark:bg-neutral-800"></div>
       </div>
-
+*/}
         <div className="hidden md:block mb-8 max-w-7xl mx-auto px-4 flex">
           <div className="w-full">
             <Image
