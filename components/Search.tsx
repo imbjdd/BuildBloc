0826bb0f685d658c50c74cb6a92d7c3f96391abb
imbjdd <b0bb1e@protonmail.com>
2024-01-19 20:19:29 +0100

    feat: real dark mode

--- components/Search.tsx
+++ components/Search.tsx
@@ -6,8 +6,8 @@ import { navigate } from './actions'
 export default function Search () {
 	return (
 		<div className="max-w-7xl mx-auto px-4 flex items-center justify-center">
-			<form action={navigate} className="md:w-1/2 flex gap-4 bg-neutral-100 dark:bg-neutral-800 p-2 rounded-xl items-center">
-				<input name="query" className="placeholder:text-neutral-600 dark:placeholder:text-neutral-300 grow h-12 rounded-xl w-1/4 px-4 md:px-8 bg-neutral-100 dark:bg-neutral-800" type="text" placeholder="Search.." />
+			<form action={navigate} className="md:w-1/2 flex gap-4 bg-neutral-100 dark:bg-neutral-900 p-2 rounded-xl items-center">
+				<input name="query" className="placeholder:text-neutral-600 dark:placeholder:text-neutral-300 grow h-12 rounded-xl w-1/4 px-4 md:px-8 bg-neutral-100 dark:bg-neutral-900" type="text" placeholder="Search.." />
 				<Dropdown />
 			</form>
 		</div>
--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,38 +1,40 @@
-'use client'
-
-import React, { useState, useEffect } from 'react';
-const { UilSun, UilMoon } = require('@iconscout/react-unicons')
-
-export default function DarkModeToggle() {
-  const [theme, setTheme] = useState(() => {
-    try {
-      const localTheme = localStorage ? localStorage.getItem('theme') : undefined;
-      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      return localTheme || (prefersDark ? 'dark' : 'not-dark');
-    } catch(e) {
-
-    }
-  });
-
-  useEffect(() => {
-    localStorage.setItem('theme', theme||'');
-    const root = document.getElementsByTagName('html')[0];
-    root.classList.toggle('dark', theme === 'dark');
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
-  };
-
+"use client"
+ 
+import * as React from "react"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+ 
+export default function ModeToggle() {
+  const { setTheme } = useTheme()
+ 
   return (
-<div className="flex flex-col justify-center"
-          onClick={toggleTheme}>
-    <input type="checkbox" name="light-switch" className="light-switch sr-only" />
-    <label className="relative cursor-pointer p-2" htmlFor="light-switch">
-        <UilSun className="dark:hidden" size="24" color="#171717" />
-        <UilMoon className="hidden dark:block" size="24" color="#F5F5F5" />
-        <span className="sr-only">Switch to light / dark version</span>
-    </label>
-</div>
-  );
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
